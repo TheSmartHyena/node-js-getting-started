d6055278e1e49b8ef6e8a0523b7561719580151e
skopal philippe <skopal.philippe@gmail.com>
2020-03-17 18:56:08 +0100

    rf

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
 
+const { Op } = require("sequelize");
+
+app.use(express.json());
+
+const modelRobot = require('./models/robot.js');
+const routeRobot = require('./routes/robot.js');
+app.models = {};
+
+async function initModels(app){
+    await modelRobot.initRobot(app);
+}
+
+async function initRoutes(app){
+    await routeRobot.initRobot(app);
+}
+
+initModels(app);
+initRoutes(app);
+
 app.get('/', (req, res) => res.json({key:"value"}))
+
+app.get('/users', (req, res) => {
+    res.json({"msg":"users"});
+})
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
